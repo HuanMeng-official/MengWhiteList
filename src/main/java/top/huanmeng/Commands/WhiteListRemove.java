6e835270242866fbd5ab64526e434a9097a459ff
HuanMeng-official <3099044113@qq.com>
2023-06-04 16:15:12 +0800

    修改包名，规范代码

---
+++ src/main/java/top/huanmeng/Commands/WhiteListRemove.java
@@ -0,0 +1,51 @@
+package top.huanmeng.Commands;
+
+import top.huanmeng.MengWhiteList;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+import static top.huanmeng.MengWhiteList.m;
+
+import java.util.List;
+import java.util.Objects;
+
+public class WhiteListRemove implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if(!sender.isOp()){
+            sender.sendMessage(m + ChatColor.RED + "该命令只能由管理员执行");
+            return false;
+        }
+        if(args.length != 1){
+            sender.sendMessage(m + ChatColor.RED + "Used: /mwl_remove <player>");
+            return false;
+        }
+        Plugin config = MengWhiteList.getPlugin(MengWhiteList.class);
+        List<String> whitelist = config.getConfig().getStringList("players");
+        if(whitelist.contains(args[0])){
+            Player remove = Bukkit.getPlayer(args[0]);
+            whitelist.remove(args[0]);
+            config.getConfig().set("players", whitelist);
+            config.saveConfig();
+            sender.sendMessage(m + ChatColor.WHITE + "已取消玩家白名单");
+            if(remove == null){
+                return false;
+            }
+            if(remove.isOnline()){
+                sender.sendMessage(m + ChatColor.WHITE + "已取消玩家白名单");
+                remove.kickPlayer(ChatColor.translateAlternateColorCodes('&',
+                                Objects.requireNonNull(config.getConfig().getString("tips"))
+                        )
+                );
+            }
+        } else {
+            sender.sendMessage(m + ChatColor.RED + "该玩家不在白名单中，请检查玩家名是否输入正确");
+        }
+        return false;
+    }
+}
---
+++ src/main/java/top/huanmeng/MengWhiteList.java
@@ -0,0 +1,44 @@
+package top.huanmeng;
+
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+import top.huanmeng.Commands.WhiteListAdd;
+import top.huanmeng.Commands.WhiteListRemove;
+import top.huanmeng.Events.JoinEvent;
+
+import java.util.Objects;
+
+public final class MengWhiteList extends JavaPlugin {
+    public static String m = ChatColor.YELLOW + "[幻梦娘]";
+    @Override
+    public void onLoad() {
+        getLogger().info(ChatColor.DARK_RED + "MengWhiteList is loading");
+    }
+
+    @Override
+    public void onEnable() {
+        getLogger().info(ChatColor.DARK_GREEN + "=========================");
+        getLogger().info(ChatColor.GOLD + "__  ____       ____");
+        getLogger().info(ChatColor.GOLD + "|  \\/  \\ \\     / /| |");
+        getLogger().info(ChatColor.GOLD + "| |\\/| |\\ \\ /\\ / /| |");
+        getLogger().info(ChatColor.GOLD + "| |  | | \\ V  V / | |___");
+        getLogger().info(ChatColor.GOLD + "|_|  |_|  \\_/\\_/   |_____|");
+        getConfig().options().copyDefaults(true);
+        saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(new JoinEvent(),this);
+        Objects.requireNonNull(getCommand("mwl_add")).setExecutor(new WhiteListAdd());
+        Objects.requireNonNull(getCommand("mwl_remove")).setExecutor(new WhiteListRemove());
+        boolean enable = getConfig().getBoolean("enable",true);
+        if (enable){
+            getLogger().info(m + ChatColor.RESET + "功能“WhiteList”已开启");
+        }else {
+            getLogger().info(m + ChatColor.RESET + "功能“WhiteList”已开启");
+        }
+        getLogger().info(ChatColor.DARK_GREEN + "=========================");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info(ChatColor.DARK_RED + "MengWhiteList is disable");
+    }
+}
