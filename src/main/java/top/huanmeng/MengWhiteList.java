934490002688d1efe62723d0da89123dd2d2ed3d
HUanMeng-official <3099044113@qq.com>
2023-08-31 18:07:56 +0800

    重新上传规范代码

---
+++ src/main/java/top/huanmeng/MengWhiteList.java
@@ -0,0 +1,50 @@
+package top.huanmeng;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+import top.huanmeng.commands.*;
+import top.huanmeng.events.JoinEvent;
+import top.huanmeng.tools.Metrics;
+
+import java.util.Objects;
+import java.util.logging.Logger;
+
+public final class MengWhiteList extends JavaPlugin {
+    public static String m = ChatColor.YELLOW + "[幻梦娘]";
+    public static Logger logger = Bukkit.getLogger();
+    @Override
+    public void onLoad() {
+        logger.info(ChatColor.DARK_RED + "MengWhiteList is loading");
+    }
+
+    @Override
+    public void onEnable() {
+        logger.info(ChatColor.DARK_GREEN + "=========================");
+        logger.info(ChatColor.GOLD + "__  ____       ____");
+        logger.info(ChatColor.GOLD + "|  \\/  \\ \\     / /| |");
+        logger.info(ChatColor.GOLD + "| |\\/| |\\ \\ /\\ / /| |");
+        logger.info(ChatColor.GOLD + "| |  | | \\ V  V / | |___");
+        logger.info(ChatColor.GOLD + "|_|  |_|  \\_/\\_/   |_____|");
+        int pluginId = 18850;
+        Metrics metrics = new Metrics(this, pluginId);
+        metrics.addCustomChart(new Metrics.SimplePie("chart_id", () -> "My value"));
+        getConfig().options().copyDefaults(true);
+        saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(new JoinEvent(),this);
+        Objects.requireNonNull(getCommand("mwl_add")).setExecutor(new WhiteListAdd());
+        Objects.requireNonNull(getCommand("mwl_remove")).setExecutor(new WhiteListRemove());
+        boolean enable = getConfig().getBoolean("enable",true);
+        if (enable){
+            logger.info(m + ChatColor.RESET + "功能“WhiteList”已开启");
+        }else {
+            logger.info(m + ChatColor.RESET + "功能“WhiteList”已关闭");
+        }
+        logger.info(ChatColor.DARK_GREEN + "=========================");
+    }
+
+    @Override
+    public void onDisable() {
+        logger.info(ChatColor.DARK_RED + "MengWhiteList is disable");
+    }
+}
